145acc9cd7a9e1bd4fc5e53e145866e04584518d
Zyad <zezozoz131@gmail.com>
2026-03-04 06:56:59 +0200

    first

---
+++ main.js
@@ -0,0 +1,48 @@
+const API_KEY = "989dc5c9";
+
+btn = document.getElementById("search");
+
+const my_movies = document.getElementById("movies");
+
+const loading = document.getElementById("loading");
+btn.onclick = function () {
+  let name = document.getElementById("search_bar").value;
+  loading.style.display = "block";
+  my_movies.innerHTML = `<h3 class = "loading"> Loading... </h3>`;
+
+  fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${name}`)
+    .then((res) => res.json())
+    .then((data) => {
+      setTimeout(() => {
+        loading.style.display = "none";
+        loading.style.color = "white";
+        if (data.Search) {
+           my_movies.innerHTML = "";
+          my_movies.classList.remove("movies");
+          my_movies.classList.add("moviesaftersearch");
+          data.Search.forEach((movie) => {
+            // if the movie has a photo
+            if (movie.Poster !== "N/A") {
+              my_movies.innerHTML += `
+            <div class="card">
+                <img src="${movie.Poster}">
+                <h3>${movie.Title}</h3>
+                <p>${movie.Year}</p>
+            </div>
+            `;
+            }
+          });
+        } else {
+            alert("Movie Not Found");
+        }
+      }, 2000);
+    });
+};
+
+// press enter
+
+document.getElementById("search_bar").addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    btn.click();
+  }
+});
